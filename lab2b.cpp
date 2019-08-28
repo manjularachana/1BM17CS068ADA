faecd452055bd0697e72d29908a50aef0ea15849
manjularachana <52826153+manjularachana@users.noreply.github.com>
2019-08-28 11:35:02 +0530

    updated alng with comparisions

--- lab2b.cpp
+++ lab2b.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-int n,k,a[10],c;
+int n,k,a[10],c,count=0;
 cout<<"Enter the size of the array";
 cin>>n;
 cout<<"how many largest elements you want??";
@@ -11,6 +11,7 @@ for(int i=0;i<n;i++){
 cin>>a[i];}
 for(int i=0;i<k;i++){
 for(int j=0;j<n-i-1;j++){
+count++;
 if(a[j]>a[j+1]){
 c=a[j];
 a[j]=a[j+1];
@@ -18,6 +19,8 @@ a[j+1]=c;
 }
 }
 }
+cout<<"number of comparisions here will be depending on k or else bubble sort has got n(n-1)/2 comparisions:"<<count;
+cout<<"\n";
 for(int i=n-1;i>=(n-k);i--){
 cout<<a[i]<<" ";
 }
