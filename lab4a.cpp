9f74d5d649e9babf281fbb581ea6179bad16ee04
manjularachana <52826153+manjularachana@users.noreply.github.com>
2019-10-14 11:11:52 +0530

    connected components

--- lab4a.cpp
+++ lab4a.cpp
@@ -1,50 +1,54 @@
-#include<iostream>
-#include<list>
+#include<bits/stdc++.h>
 using namespace std;
-class graph{
-int v;
-list<int>*gra;
-void dfs(int v,bool visited[]);
-public:
-graph(int v);
-void addedge(int v,int w);
-void graphcompo();
-};
-graph::graph(int a){
-this->v=a;
-gra=new list<int>[v];
+
+// A utility function to add an edge in an
+// undirected graph.
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
 }
-void graph::addedge(int v,int w){
-gra[v].push_back(w);
-gra[w].push_back(v);
+
+// A utility function to do DFS of graph
+// recursively from a given vertex u.
+void DFSUtil(int u, vector<int> adj[],
+                    vector<bool> &visited)
+{
+    visited[u] = true;
+    cout << u << " ";
+    for (int i=0; i<adj[u].size(); i++)
+        if (visited[adj[u][i]] == false){
+            DFSUtil(adj[u][i], adj, visited);
+            cout<<"\n";
+          }
 }
-void graph::graphcompo(){
-bool *visited=new bool[v];
-for(int i=0;i<v;i++){
-visited[i]=false;
+
+// This function does DFSUtil() for all
+// unvisited vertices.
+void DFS(vector<int> adj[], int V)
+{
+    vector<bool> visited(V, false);
+    for (int u=0; u<V; u++)
+        if (visited[u] == false)
+            DFSUtil(u, adj, visited);
 }
-for(int j=0;j<v;j++){
-if(!visited[j]){
-dfs(j,visited);
-cout<<"\n";
+
+// Driver code
+int main()
+{
+    int V = 5;
+
+    // The below line may not work on all
+    // compilers.  If it does not work on
+    // your compiler, please replace it with
+    // following
+    // vector<int> *adj = new vector<int>[V];
+    vector<int> adj[V];
+
+    // Vertex numbers should be from 0 to 4.
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 3, 4);
+    DFS(adj, V);
+    return 0;
 }
-}
-}
-void graph::dfs(int v,bool visited[]){
-visited[v]=true;
-cout<<v<<" ";
-list<int>::iterator it;
-for(it=gra[v].begin();it!=gra[v].end();it++){
-if(!visited[*it]){
-dfs(*it,visited);
-}
-}
-}
-int main(){
-graph g=graph(5);
-g.addedge(1,0);
-g.addedge(1,2);
-g.addedge(2,3);
-g.addedge(3,4);
-g.graphcompo();
-return 0;}
